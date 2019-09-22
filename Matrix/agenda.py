7281ec0b4f121842dea9fecdde1fc0a37aa14c4e
Mr. Roth-Ritchie <jrothritchie@hdsd.org>
2019-09-22 17:04:31 -0400

    Updated the logic to read a full agenda for one class.

--- Matrix/agenda.py
+++ Matrix/agenda.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # Display a runtext with double-buffering.
-
+from samplebase import SampleBase
+#!/usr/bin/env python
 import os
 import subprocess
-from samplebase import SampleBase
+import csv
 from rgbmatrix import graphics, RGBMatrixOptions
 import textwrap
 import time
-import csv
 
 
 class RunText(SampleBase):
@@ -16,39 +16,46 @@ class RunText(SampleBase):
 
 
     def run(self):
+        # Create constants
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
         font.LoadFont("rpi-rgb-led-matrix/fonts/4x6.bdf")
         font2 = graphics.Font()
         font2.LoadFont("rpi-rgb-led-matrix/fonts/tom-thumb.bdf")
         textColor = graphics.Color(100, 100, 50)
+        textColor2 = graphics.Color(100, 20, 100)
         pos = offscreen_canvas.width
         
+	# Get the file
         with open('/mnt/flash/agenda.txt') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             data=list(csv.reader(csv_file, delimiter=','))
-
+	
+        print (data)
         agenda = data[1][2]
         Objective = data[1][1]
-        Categories = data[1]
+        Categories = data[0]
+        homework = data[1][3]
+        
         h= 6 # Font Height
         pos1 = h+1
         pos = h+1
-        new_pos = 6
+        new_pos = h
         line = 1
         print('agenda is' +agenda)
         print(type(agenda))
+        Objective = tuple(Objective.splitlines(0))
         agenda = agenda.splitlines(0)
         print(type(agenda))
         new_agenda = []
         for x in range(len(agenda)):
-            wrapper = textwrap.fill(agenda [x], 6)
+            wrapper = textwrap.fill(agenda [x], 18)
             w2 = "\n".join(wrapper.split("\n"))
             new_agenda.append(w2)
         a = '\n'.join(new_agenda)
         print(a)
         a = a.splitlines(0)
-        Objective = tuple(Objective.splitlines(0))
+        homework = homework.splitlines(0)
         aLength = int((len(agenda)) / 3)
         print (a)
         count = 0
@@ -56,12 +63,22 @@ class RunText(SampleBase):
 
         
         while True:
-            #offscreen_canvas.clear()
+            # Objective
             h1 = Categories[1]
-            stuff = graphics.DrawText(offscreen_canvas, font2, 0 , pos, textColor, h1)
+            stuff = graphics.DrawText(offscreen_canvas, font2, 0 , pos, textColor2, h1)
             pos += new_pos
             
+            for x in range (len(Objective)):
+                h1 = Objective[x]
+                stuff = graphics.DrawText(offscreen_canvas, font2, 0 , pos, textColor, h1)
+                pos += new_pos
+            
+            
             # Agenda Loop
+            h1 = Categories[2]
+            stuff = graphics.DrawText(offscreen_canvas, font2, 0 , pos, textColor2, h1)
+            pos += new_pos
+
             for x in range(aLength):
                 for x in range(len(a) - count):
                     # 1st display
@@ -74,29 +91,37 @@ class RunText(SampleBase):
                     #prev = x  
                     #second display
                     #if x > 3:
+                offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+                time.sleep(10)
+                offscreen_canvas.Clear()
+
+                #Homework:
+                h1 = Categories[3]
+                stuff = graphics.DrawText(offscreen_canvas, font2, 0 , pos, textColor2, h1)
+                pos += new_pos
+
+                for x in range(len(homework)):
+                    my_text = homework[x]
+                    # print(my_text)
+                    stuff = graphics.DrawText(offscreen_canvas, font, 0 ,pos, textColor, my_text)
+                    pos += new_pos
+                    print(my_text)
       
                 offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
                 count += 3
                 time.sleep(10)
-                offscreen_canvas.Clear()
             
-            count = 0
-            pos = pos1
-            h2 = Categories[1]
-            stuff2 = graphics.DrawText(offscreen_canvas, font, 0 ,pos, textColor, h2)
-            pos += new_pos
-            time.sleep(1)
-         
-            
-            for x in range(len(Objective)):
-                time.sleep(1)
-                my_text = Objective[x]
-                stuff = graphics.DrawText(offscreen_canvas, font, 0 ,pos, textColor, my_text)
-                pos += new_pos
-                print(my_text)     
-            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+ 
 
-            time.sleep(10)
+            
+            offscreen_canvas.Clear()
+            count = 0
+            pos = pos1         
+            # pos =0
+            stuff2 = graphics.DrawText(offscreen_canvas, font, 0 ,pos, textColor, "")
+            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+            time.sleep(2)
+            
 
             
 
